6fb002b871f737a1c90ef0c2c09aa7a1f73ffa77
TaherSaid <amosiwar@gmail.com>
2022-12-15 15:36:25 +0100

    Fix express middleware

--- server/index.js
+++ server/index.js
@@ -203,14 +203,7 @@ app.post(
   }
 );
 
-app.use(
-  "/",
-  cors(),
-  bodyParser.json(),
-  expressMiddleware(server, {
-    context: async ({ req }) => ({ token: req.headers.token }),
-  })
-);
+app.use("/", cors(), bodyParser.json(), expressMiddleware(server));
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 
-console.log(`🚀 Server ready at http://localhost:4000/`);
+console.log(`Server ready at http://localhost:4000/`);
